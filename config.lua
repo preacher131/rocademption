bc2c8708f2cf6cc9a37e7c4101d1a56581426ad7
preacher131 <preacher131@gmail.com>
2019-03-16 16:42:28 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -40,42 +40,42 @@ Config.Points = {
 ----------------------------------------------------
 
 -- Si steam n'est pas détecté / If steam is not detected
-Config.NoSteam = "Steam n'a pas été détecté. Veuillez (re)lancer Steam et FiveM, puis réessayer."
--- Config.NoSteam = "Steam was not detected. Please (re)launch Steam and FiveM, and try again."
+-- Config.NoSteam = "Steam n'a pas été détecté. Veuillez (re)lancer Steam et FiveM, puis réessayer."
+Config.NoSteam = "Steam was not detected. Please (re)launch Steam and FiveM, and try again."
 
 -- Message d'attente / Waiting text
-Config.EnRoute = "Vous êtes en route. Vous avez déjà parcouru"
--- Config.EnRoute = "You are on the road. You have already traveled"
+-- Config.EnRoute = "Vous êtes en route. Vous avez déjà parcouru"
+Config.EnRoute = "You are on the road. You have already traveled"
 
 -- "points" traduits en langage RP / "points" for RP purpose
-Config.PointsRP = "kilomètres"
--- Config.PointsRP = "kilometers"
+-- Config.PointsRP = "kilomètres"
+Config.PointsRP = "kilometers"
 
 -- Position dans la file / position in the queue
-Config.Position = "Vous êtes en position "
--- Config.Position = "You are in position "
+-- Config.Position = "Vous êtes en position "
+Config.Position = "You are in position "
 
 -- Texte avant les emojis / Text before emojis
-Config.EmojiMsg = "Si les emojis sont figés, relancez votre client : "
--- Config.EmojiMsg = "If the emojis are frozen, restart your client: "
+--Config.EmojiMsg = "Si les emojis sont figés, relancez votre client : "
+Config.EmojiMsg = "If the emojis are frozen, restart your client: "
 
 -- Quand le type gagne à la loterie / When the player win the lottery
-Config.EmojiBoost = "!!! Youpi, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " gagnés !!!"
--- Config.EmojiBoost = "!!! Yippee, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " won !!!"
+-- Config.EmojiBoost = "!!! Youpi, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " gagnés !!!"
+Config.EmojiBoost = "!!! Yippee, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " won !!!"
 
 -- Anti-spam message / anti-spam text
-Config.PleaseWait_1 = "Veuillez patienter "
-Config.PleaseWait_2 = " secondes. La connexion se lancera automatiquement !"
--- Config.PleaseWait_1 = "Please wait "
--- Config.PleaseWait_2 = " seconds. The connection will start automatically!"
+-- Config.PleaseWait_1 = "Veuillez patienter "
+-- Config.PleaseWait_2 = " secondes. La connexion se lancera automatiquement !"
+Config.PleaseWait_1 = "Please wait "
+Config.PleaseWait_2 = " seconds. The connection will start automatically!"
 
 -- Me devrait jamais s'afficher / Should never be displayed
-Config.Accident = "Oups, vous venez d'avoir un accident... Si cela se reproduit, vous pouvez en informer le support :)"
--- Config.Accident = "Oops, you just had an accident ... If it happens again, you can inform the support :)"
+-- Config.Accident = "Oups, vous venez d'avoir un accident... Si cela se reproduit, vous pouvez en informer le support :)"
+Config.Accident = "Oops, you just had an accident ... If it happens again, you can inform the support :)"
 
 -- En cas de points négatifs / In case of negative points
-Config.Error = " ERREUR : RELANCEZ LA ROCADE ET CONTACTEZ LE SUPPORT DU SERVEUR "
--- Config.Error = " ERROR : RESTART THE QUEUE SYSTEM AND CONTACT THE SUPPORT "
+-- Config.Error = " ERREUR : RELANCEZ LA ROCADE ET CONTACTEZ LE SUPPORT DU SERVEUR "
+Config.Error = " ERROR : RESTART THE QUEUE SYSTEM AND CONTACT THE SUPPORT "
 
 
 Config.EmojiList = {
